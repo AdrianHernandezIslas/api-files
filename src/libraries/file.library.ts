b1a711db2fcb45d24f438a0787b68f0a0a8c4b73
Adrian Hernandez Islas <hernandezadri>
2024-08-09 21:43:29 -0600

    feat: add file library

---
+++ src/libraries/file.library.ts
@@ -0,0 +1,25 @@
+import mime from "mime-types";
+
+export class FileLibrary {
+  private readonly BASE64_FORMAT = "base64";
+  private readonly BASE64_REGEX = /^data:(.+);base64,(.*)$/;
+
+  getMetadataBase64(base64Data: string) {
+    const match = base64Data.match(this.BASE64_REGEX);
+
+    if (!match) {
+      throw new Error("Formato base64 no válido");
+    }
+
+    const [, mimeType, content] = match;
+    return { mimeType, content };
+  }
+
+  base64toBuffer(content: string): Buffer {
+    return Buffer.from(content, this.BASE64_FORMAT);
+  }
+
+  getExtensionFile(mimeType:string){
+    return mime.extension(mimeType);
+  }
+}
